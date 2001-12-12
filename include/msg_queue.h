ac57ebf5fd0b42c9e14c9e9e2a0ab350e78a054d
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2001-12-12 01:15:33 +0000

    sort queue by time, fix msg_queue init mem corruption

--- include/msg_queue.h
+++ include/msg_queue.h
@@ -24,13 +24,16 @@ struct msg_queue
     int entries;
     struct msg_queue_entry *head;
     struct msg_queue_entry *tail;
+    UDWORD exp_time;
 };
 
-void        msg_queue_init (struct msg_queue **queue);
-struct msg *msg_queue_peek (struct msg_queue *queue);
-struct msg *msg_queue_pop (struct msg_queue *queue);
-void        msg_queue_push (struct msg *new_msg, struct msg_queue *queue);
+void        msg_queue_init          (struct msg_queue **queue);
+struct msg *msg_queue_peek          (struct msg_queue  *queue);
+struct msg *msg_queue_pop           (struct msg_queue  *queue);
+void        msg_queue_enqueue       (struct msg_queue  *queue, struct msg *new_msg);
+struct msg *msg_queue_dequeue_seq   (struct msg_queue  *queue, UDWORD seq);
 void        Check_Queue (UDWORD seq, struct msg_queue *queue);
-void        Dump_Queue (void);
+
+#define     msg_queue_push(m,q)   msg_queue_enqueue(q,m)
 
 #endif
--- src/msg_queue.c
+++ src/msg_queue.c
@@ -8,31 +8,29 @@ Handles resending missed packets.
 #include "util_ui.h"
 #include "util.h"
 #include "cmd_pkt_server.h"
+#include "contact.h"
 #include <stdlib.h>
 #include <assert.h>
 #include <limits.h>
 
-static struct msg_queue *queue = NULL;
-
-
 void msg_queue_init (struct msg_queue **queue)
 {
-    *queue = malloc (sizeof (*queue));
+    assert (queue);
+    
+    *queue = malloc (sizeof (**queue));
     (*queue)->entries = 0;
     (*queue)->head = (*queue)->tail = NULL;
+    (*queue)->exp_time = INT_MAX;
 }
 
 
 struct msg *msg_queue_peek (struct msg_queue *queue)
 {
-    if (NULL != queue->head)
-    {
+    assert (queue);
+
+    if (queue->head)
         return queue->head->msg;
-    }
-    else
-    {
-        return NULL;
-    }
+    return NULL;
 }
 
 
@@ -41,129 +39,137 @@ struct msg *msg_queue_pop (struct msg_queue *queue)
     struct msg *popped_msg;
     struct msg_queue_entry *temp;
 
-    if ((NULL != queue->head) && (queue->entries > 0))
+    assert (queue);
+
+    if (queue->head && (queue->entries > 0))
     {
         popped_msg = queue->head->msg;
         temp = queue->head->next;
         free (queue->head);
         queue->head = temp;
         queue->entries--;
-        if (NULL == queue->head)
+        if (!queue->head)
         {
             queue->tail = NULL;
+            queue->exp_time = INT_MAX;
         }
-        /* if ( 0x1000 < Chars_2_Word( &popped_msg->body[6] ) ) {
-           M_print( "\n\n******************************************\a\a\a\n"
-           "Error!!!!!!!!!!!!!!!!!!!!!\n" );
-           } */
+        else
+            queue->exp_time = queue->head->msg->exp_time;
         return popped_msg;
     }
-    else
-    {
-        return NULL;
-    }
+    return NULL;
 }
 
 
-void msg_queue_push (struct msg *new_msg, struct msg_queue *queue)
+void msg_queue_enqueue (struct msg_queue *queue, struct msg *new_msg)
 {
     struct msg_queue_entry *new_entry;
+    struct msg_queue_entry *iter_entry;
 
-    assert (NULL != new_msg);
-
-    if (NULL == queue)
-        return;
+    assert (queue);
+    assert (new_msg);
 
     new_entry = malloc (sizeof (struct msg_queue_entry));
-    if (new_entry == NULL)
-    {
-        M_print (i18n (617, "Memory exhausted!!\a\n"));
-        exit (-1);
-    }
+
+    assert (new_entry);
+
     new_entry->next = NULL;
     new_entry->msg = new_msg;
 
-    if (queue->tail)
+    if (!queue->head || !queue->tail)
+    {
+        queue->head = queue->tail = new_entry;
+    }
+    else if (new_msg->exp_time <= queue->head->msg->exp_time)
+    {
+        new_entry->next = queue->head;
+        queue->head = new_entry;
+    }
+    else if (new_msg->exp_time > queue->tail->msg->exp_time)
     {
         queue->tail->next = new_entry;
         queue->tail = new_entry;
     }
     else
     {
-        queue->head = queue->tail = new_entry;
+        for (iter_entry = queue->head; new_msg->exp_time > iter_entry->next->msg->exp_time; iter_entry = iter_entry->next)
+        {
+           assert (iter_entry->next);
+           assert (iter_entry->next->next);
+        }
+        new_entry->next = iter_entry->next;
+        iter_entry->next = new_entry;
     }
-
     queue->entries++;
+    queue->exp_time = queue->head->msg->exp_time;
 }
 
-void Dump_Queue (void)
+struct msg *msg_queue_dequeue_seq (struct msg_queue *queue, UDWORD seq)
 {
-    int i;
     struct msg *queued_msg;
+    struct msg_queue_entry *iter_entry;
+    struct msg_queue_entry *tmp_entry;
 
-    assert (queue != NULL);
+    assert (queue);
     assert (0 <= queue->entries);
 
-    M_print (i18n (618, "\nTotal entries %d\n"), queue->entries);
-    for (i = 0; i < queue->entries; i++)
+    if (queue->head && queue->head->msg->seq == seq)
     {
-        queued_msg = msg_queue_pop (queue);
-        M_print (i18n (619, "SEQ = %04x\tCMD = %04x\tattempts = %d\tlen = %d\n"),
-                 (queued_msg->seq >> 16), Chars_2_Word (&queued_msg->body[CMD_OFFSET]),
-                 queued_msg->attempts, queued_msg->len);
-        if (uiG.Verbose & 16)
+        tmp_entry = queue->head;
+        queued_msg = tmp_entry->msg;
+        queue->head = queue->head->next;
+        free (tmp_entry);
+        if (!queue->head)
         {
-            Hex_Dump (queued_msg->body, queued_msg->len);
+            queue->tail = NULL;
+            queue->exp_time = INT_MAX;
         }
-        msg_queue_push (queued_msg, queue);
+        else
+            queue->exp_time = queue->head->msg->exp_time;
+        return queued_msg;
     }
+    for (iter_entry = queue->head; iter_entry->next; iter_entry = iter_entry->next)
+    {
+        if (iter_entry->next->msg->seq == seq)
+        {
+            tmp_entry = iter_entry->next;
+            queued_msg = tmp_entry->msg;
+            iter_entry->next=iter_entry->next->next;
+            free (tmp_entry);
+            if (queue->tail == tmp_entry)
+                queue->tail = iter_entry;
+            return queued_msg;
+        }
+    }
+    return NULL;
 }
 
 void Check_Queue (UDWORD seq, struct msg_queue *queue)
 {
-    int i;
-    struct msg *queued_msg;
+    struct msg *queued_msg = msg_queue_dequeue_seq (queue, seq);
 
-    assert (0 <= queue->entries);
+    if (!queued_msg)
+        return;
 
-    for (i = 0; i < queue->entries; i++)
+    if (uiG.Verbose & 16)
     {
-        queued_msg = msg_queue_pop (queue);
-        if (queued_msg->seq != seq)
-        {
-            msg_queue_push (queued_msg, queue);
-        }
-        else
-        {
-            if (uiG.Verbose & 16)
-            {
-                UDWORD tmp = Chars_2_Word (&queued_msg->body[CMD_OFFSET]);
-                R_undraw ();
-                M_print (i18n (824, "Acknowledged packet type %04x (%s) sequence %04x removed from queue.\n"),
-                         tmp, CmdPktSrvName (tmp), queued_msg->seq >> 16);
-                R_redraw ();
-            }
-            if (Chars_2_Word (&queued_msg->body[CMD_OFFSET]) == CMD_SENDM)
-            {
-                R_undraw ();
-                Time_Stamp ();
-                M_print (" " COLACK "%10s" COLNONE " %s%s\n",
-                         UIN2Name (Chars_2_DW (&queued_msg->body[PAK_DATA_OFFSET])),
-                         MSGACKSTR, MsgEllipsis (&queued_msg->body[32]));
-                R_redraw ();
-            }
-            free (queued_msg->body);
-            free (queued_msg);
-
-            if ((queued_msg = msg_queue_peek (queue)) != NULL)
-            {
-                ssG.next_resend = queued_msg->exp_time;
-            }
-            else
-            {
-                ssG.next_resend = INT_MAX;
-            }
-            break;
-        }
+        UDWORD tmp = Chars_2_Word (&queued_msg->body[CMD_OFFSET]);
+        R_undraw ();
+        M_print (i18n (824, "Acknowledged packet type %04x (%s) sequence %04x removed from queue.\n"),
+                 tmp, CmdPktSrvName (tmp), queued_msg->seq >> 16);
+        R_redraw ();
     }
+    if (Chars_2_Word (&queued_msg->body[CMD_OFFSET]) == CMD_SENDM)
+    {
+        R_undraw ();
+        Time_Stamp ();
+        M_print (" " COLACK "%10s" COLNONE " %s%s\n",
+                 ContactFindName (Chars_2_DW (&queued_msg->body[PAK_DATA_OFFSET])),
+                 MSGACKSTR, MsgEllipsis (&queued_msg->body[32]));
+        R_redraw ();
+    }
+    free (queued_msg->body);
+    free (queued_msg);
+
+    ssG.next_resend = queue->exp_time;
 }
