0cf8c9f805551af3876fab12584a982fb3c0726d
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2002-05-01 14:57:11 +0000

    packet.c cleanup

--- include/packet.h
+++ include/packet.h
@@ -27,7 +27,7 @@ void        PacketWrite4      (      Packet *pak,           UDWORD data);
 void        PacketWriteB4     (      Packet *pak,           UDWORD data);
 void        PacketWriteData   (      Packet *pak,           const char *data, UWORD len);
 void        PacketWriteStrB   (      Packet *pak,           const char *data);
-void        PacketWriteStrN   (      Packet *pak,           const char *data);
+void        PacketWriteLNTS   (      Packet *pak,           const char *data);
 void        PacketWriteStrCUW (      Packet *pak,           const char *data);
 void        PacketWriteUIN    (      Packet *pak, UDWORD uin);
 UWORD       PacketWritePos    (const Packet *pak);
@@ -41,10 +41,10 @@ UWORD       PacketRead2       (      Packet *pak);
 UWORD       PacketReadB2      (      Packet *pak);
 UDWORD      PacketRead4       (      Packet *pak);
 UDWORD      PacketReadB4      (      Packet *pak);
-void        PacketReadSkip    (      Packet *pak,                       UWORD len);
+const char *PacketReadSkip    (      Packet *pak,                       UWORD len);
 void        PacketReadData    (      Packet *pak,           char *data, UWORD len);
 char       *PacketReadStrB    (      Packet *pak);
-const char *PacketReadStrN    (      Packet *pak);
+const char *PacketReadLNTS    (      Packet *pak);
 UDWORD      PacketReadUIN     (      Packet *pak);
 UWORD       PacketReadPos     (const Packet *pak);
 UBYTE       PacketReadAt1     (const Packet *pak, UWORD at);
@@ -53,14 +53,14 @@ UWORD       PacketReadBAt2    (const Packet *pak, UWORD at);
 UDWORD      PacketReadAt4     (const Packet *pak, UWORD at);
 UDWORD      PacketReadBAt4    (const Packet *pak, UWORD at);
 void        PacketReadAtData  (const Packet *pak, UWORD at, char *data, UWORD len);
-const char *PacketReadAtStrB  (const Packet *pak, UWORD at);
-const char *PacketReadAtStrN  (const Packet *pak, UWORD at);
+char       *PacketReadAtStrB  (const Packet *pak, UWORD at);
+const char *PacketReadAtLNTS  (const Packet *pak, UWORD at);
 int         PacketReadLeft    (const Packet *pak);
 
-#define PacketWriteTLV2(pak,tlv,data)        do { PacketWriteB2 (pak,tlv); PacketWriteB2 (pak, 2); PacketWriteB2 (pak, data); } while (0)
-#define PacketWriteTLV4(pak,tlv,data)        do { PacketWriteB2 (pak,tlv); PacketWriteB2 (pak, 4); PacketWriteB4 (pak, data); } while (0)
-#define PacketWriteTLVData(pak,tlv,data,len) do { PacketWriteB2 (pak,tlv); PacketWriteB2 (pak, len); PacketWriteData (pak, data, len); } while (0)
-#define PacketWriteTLVStr(pak,tlv,data)      do { PacketWriteB2 (pak,tlv); PacketWriteStrB (pak, data); } while (0)
-#define PacketWriteTLVUIN(pak,tlv,data)      do { PacketWriteB2 (pak,tlv); PacketWriteUIN (pak, data); } while (0)
+#define PacketWriteTLV2(pak,tlv,data)        do { PacketWriteB2 (pak, tlv); PacketWriteB2   (pak, 2);   PacketWriteB2 (pak, data);        } while (0)
+#define PacketWriteTLV4(pak,tlv,data)        do { PacketWriteB2 (pak, tlv); PacketWriteB2   (pak, 4);   PacketWriteB4 (pak, data);        } while (0)
+#define PacketWriteTLVData(pak,tlv,data,len) do { PacketWriteB2 (pak, tlv); PacketWriteB2   (pak, len); PacketWriteData (pak, data, len); } while (0)
+#define PacketWriteTLVStr(pak,tlv,data)      do { PacketWriteB2 (pak, tlv); PacketWriteStrB (pak, data);                                  } while (0)
+#define PacketWriteTLVUIN(pak,tlv,data)      do { PacketWriteB2 (pak, tlv); PacketWriteUIN  (pak, data);                                  } while (0)
 
 #endif /* MICQ_PACKET_H */
