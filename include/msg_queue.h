d612451ca81ac6f122ba46108fb3431440b58887
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2002-08-07 12:32:02 +0000

    add queue type for file transfer, renumber queue types

--- include/msg_queue.h
+++ include/msg_queue.h
@@ -36,13 +36,14 @@ void          QueueRun         (struct Queue  *queue);
 struct Event *QueuePeek        (struct Queue  *queue);
 struct Event *QueuePop         (struct Queue  *queue);
 
-#define QUEUE_TYPE_UDP_RESEND    23
+#define QUEUE_TYPE_PEER_RESEND   45
+#define QUEUE_TYPE_UDP_RESEND    43
 #define QUEUE_TYPE_TCP_RESEND    42
-#define QUEUE_TYPE_TCP_TIMEOUT   76
+#define QUEUE_TYPE_TCP_TIMEOUT   72
 #define QUEUE_TYPE_CON_TIMEOUT   77
-#define QUEUE_TYPE_TCP_RECEIVE   31
-#define QUEUE_TYPE_UDP_KEEPALIVE 20
+#define QUEUE_TYPE_TCP_RECEIVE   32
+#define QUEUE_TYPE_UDP_KEEPALIVE 23
 #define QUEUE_TYPE_SRV_KEEPALIVE 21
-#define QUEUE_TYPE_FLAC          11
+#define QUEUE_TYPE_FLAC          34
 
 #endif
