8b8dad0594e8e49ad17df375b057491d21d2df4c
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2002-12-08 20:33:46 +0000

    fix koi8 / win1251 buildin conversion

--- src/conv.c
+++ src/conv.c
@@ -328,7 +328,8 @@ const UDWORD koi8u_utf8[] = { /* 7bit are us-ascii */
     0x042e, 0x0410, 0x0411, 0x0426, 0x0414, 0x0415, 0x0424, 0x0413,
     0x0425, 0x0418, 0x0419, 0x041a, 0x041b, 0x041c, 0x041d, 0x041e,
     0x041f, 0x042f, 0x0420, 0x0421, 0x0422, 0x0423, 0x0416, 0x0412,
-    0x042c, 0x042b, 0x0417, 0x0428, 0x042d, 0x0429, 0x0427, 0x042a
+    0x042c, 0x042b, 0x0417, 0x0428, 0x042d, 0x0429, 0x0427, 0x042a,
+    0x0
 };
 
 const UDWORD win1251_utf8[] = { /* 7bit are us-ascii */
@@ -347,7 +348,8 @@ const UDWORD win1251_utf8[] = { /* 7bit are us-ascii */
     0x0430, 0x0431, 0x0432, 0x0433, 0x0434, 0x0435, 0x0436, 0x0437,
     0x0438, 0x0439, 0x043a, 0x043b, 0x043c, 0x043d, 0x043e, 0x043f,
     0x0440, 0x0441, 0x0442, 0x0443, 0x0444, 0x0445, 0x0446, 0x0447,
-    0x0448, 0x0449, 0x044a, 0x044b, 0x044c, 0x044d, 0x044e, 0x044f
+    0x0448, 0x0449, 0x044a, 0x044b, 0x044c, 0x044d, 0x044e, 0x044f,
+    0x0
 };
 
 const char *ConvToUTF8 (const char *inn, UBYTE enc)
@@ -508,22 +510,28 @@ const char *ConvFromUTF8 (const char *inn, UBYTE enc)
                     }
                 continue;
             case ENC_KOI8:
-                for (i = 0; i < 128; i++)
+                for (i = 0; i <= 128; i++)
+                {
                     if (koi8u_utf8[i] == val)
                     {
                         t = s_catf (t, &size, "%c", (UBYTE)(i + 128));
-                        continue;
+                        break;
                     }
-                t = s_catf (t, &size, "?");
+                    if (i == 128)
+                        t = s_catf (t, &size, "?");
+                }
                 continue;
             case ENC_WIN1251:
-                for (i = 0; i < 128; i++)
+                for (i = 0; i <= 128; i++)
+                {
                     if (win1251_utf8[i] == val)
                     {
                         t = s_catf (t, &size, "%c", (UBYTE)(i + 128));
-                        continue;
+                        break;
                     }
-                t = s_catf (t, &size, "?");
+                    if (i == 128)
+                        t = s_catf (t, &size, "?");
+                }
                 continue;
 #if 0
             case ENC_EUC:
