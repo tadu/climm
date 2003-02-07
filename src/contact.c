9bdfb8cb2a32e0feb97cd27ca229a12e448eede6
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2003-02-07 23:39:31 +0000

    special case around SIM 0.8.x, which doesn't understand UTF-8 though it claims so

--- src/contact.c
+++ src/contact.c
@@ -731,6 +731,8 @@ void ContactSetCap (Contact *cont, Cap *cap)
             cont->v1 = (ver >> 6) - 1;
             cont->v2 = ver & 0x1f;
             cont->v3 = cont->v4 = 0;
+            if (ver <= 0x108)
+                cont->caps &= ~(1 << CAP_UTF8);
         }
         else /* KOtzPEKE */
         {
