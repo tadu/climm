b06fe4a6ee9b872b8b182cabc2c392a04779d50f
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2003-05-12 14:38:39 +0000

    add loads of GPLv2 notices, use numerical platform id

--- src/cmd_pkt_v8_snac.c
+++ src/cmd_pkt_v8_snac.c
@@ -1,8 +1,21 @@
 /*
  * Handles incoming and creates outgoing SNAC packets.
  *
- * This file is Copyright © Rüdiger Kuhlmann; it may be distributed under
- * version 2 of the GPL licence.
+ * mICQ Copyright (C) © 2001,2002,2003 Rüdiger Kuhlmann
+ *
+ * mICQ is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 dated June, 1991.
+ *
+ * mICQ is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this package; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
  *
  * $Id$
  */
@@ -1428,7 +1441,7 @@ void SnacCliSetstatus (Connection *conn, UDWORD status, UWORD action)
         PacketWriteB2 (pak, 3);
         PacketWriteB4 (pak, BUILD_MICQ);
         PacketWriteB4 (pak, BuildVersionNum);
-        PacketWriteB4 (pak, time (NULL));
+        PacketWriteB4 (pak, BuildPlatformID);
         PacketWriteB2 (pak, 0);
         PacketWriteTLV2 (pak, 8, 0);
     }
--- src/msg_queue.c
+++ src/msg_queue.c
@@ -1,8 +1,21 @@
 /*
  * Provides a time sorted queue, with callback for due events.
  *
- * This file is Copyright © Rüdiger Kuhlmann; it may be distributed under
- * version 2 of the GPL licence.
+ * mICQ Copyright (C) © 2001,2002,2003 Rüdiger Kuhlmann
+ *
+ * mICQ is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 dated June, 1991.
+ *
+ * mICQ is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this package; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
  *
  * This file replaces a simple FIFO implementation for a similar,
  * but more limited, purpose by Lawrence Gold.
