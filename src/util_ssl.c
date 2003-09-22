42395e89ad88c1144763cfab2b9d3bb689d208af
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2003-09-22 01:22:34 +0000

    we need the contact for TCL. Ooops.

--- src/util_ssl.c
+++ src/util_ssl.c
@@ -172,6 +172,7 @@ int ssl_connect (Connection *conn, BOOL is_client)
 {
     int ret;
     int kx_prio[2] = { GNUTLS_KX_ANON_DH, 0 };
+    Contact *contact = ContactUIN (conn, conn->uin);    
 
     Debug (DEB_SSL, "ssl_connect");
     if (!ssl_init_ok || (conn->ssl_status != SSL_STATUS_NA && conn->ssl_status != SSL_STATUS_INIT && conn->ssl_status != SSL_STATUS_REQUEST))
