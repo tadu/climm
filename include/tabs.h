243167aff626528ee997fb75d8e4c74728695464
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2004-07-03 18:08:35 +0000

    corrected (hopefully) tabbing, add time stamps to tabs command

--- include/tabs.h
+++ include/tabs.h
@@ -7,6 +7,7 @@ void           TabInit (void);
 void           TabAddIn  (const Contact *cont);
 void           TabAddOut (const Contact *cont);
 const Contact *TabGet    (int nr);
+time_t         TabTime   (int nr);
 int            TabHas    (const Contact *cont);
 
 #endif /* MICQ_UTIL_TABS_H */
