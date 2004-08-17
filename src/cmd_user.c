e3f7a4260a7e71ec5b3808971705da82ad5824d6
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2004-08-17 20:23:07 +0000

    renumber due to changed format string

--- src/cmd_user.c
+++ src/cmd_user.c
@@ -1518,7 +1518,7 @@ static JUMP_F(CmdUserStatusDetail)
                 free (t2);
             }
             if (cont->group && cont->group != conn->contacts && cont->group->name)
-                rl_printf ("    %s %s\n", i18n (2404, "Group:"), cont->group->name);
+                rl_printf ("    %s %s\n", i18n (2536, "Group:"), cont->group->name);
             for (j = id = 0; id < CAP_MAX; id++)
                 if (cont->caps & (1 << id))
                 {
