27ac2546b96cc419b08df2cf370b2e9d526008a4
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2004-12-21 20:11:19 +0000

    remove useless typedef

--- include/datatype.h
+++ include/datatype.h
@@ -58,7 +58,6 @@ typedef int FD_T;
 typedef int SOK_T;
 
 #ifdef _WIN32
-  typedef unsigned int ssize_t;
   #define sockread(s,p,l)  recv (s, (char *) p, l, 0)
   #define sockwrite(s,p,l) send (s, (char *) p, l, 0)
   #define sockclose(s)     closesocket(s)
