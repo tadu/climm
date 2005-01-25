24bf70fa5f1692fcd85255aca6fdcdcb2a3bf4df
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2005-01-25 23:01:45 +0000

    fix %s order

--- include/cmd_user.h
+++ include/cmd_user.h
@@ -39,7 +39,7 @@ void CmdUserInterrupt (void);
 void CmdUserCallbackTodo (Event *event);
 
 #define CMD_USER_HELP(syn,des) rl_printf ("%s" syn "%s\n\t" COLINDENT "%s" COLEXDENT "\n", COLQUOTE, COLNONE, des)
-#define CMD_USER_HELP3(syn,d,e,f) rl_printf ("%s" syn "%s\n\t" COLINDENT "%s" COLEXDENT "\n", COLQUOTE, COLNONE, d, e, f)
+#define CMD_USER_HELP3(syn,d,e,f) rl_printf ("%s" syn "%s\n\t" COLINDENT "%s" COLEXDENT "\n", COLQUOTE, d, e, COLNONE, f)
 #define CMD_USER_HELP7(syn,a,b,c,d,e,f,g) rl_printf ("%s" syn "%s\n\t" COLINDENT "%s\n%s\n%s\n%s\n%s\n%s\n%s" COLEXDENT "\n", COLQUOTE, COLNONE, a,b,c,d,e,f,g)
 
 #endif /* MICQ_USER_H */
