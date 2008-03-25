68175a5b7e79257e3efcc408d50fc052435a824b
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2008-03-25 15:28:31 +0000

    recognise iChat

--- src/jabber_base.cpp
+++ src/jabber_base.cpp
@@ -632,6 +632,8 @@ void CLIMMXMPP::handleXEP115 (gloox::Tag *t, Contact *contr)
             node = "Psi";
         else if (!strcmp (node.c_str(), "http://miranda-im.org/caps"))
             node = "Miranda";
+        else if (!strcmp (node.c_str(), "apple:ichat:caps"))
+            node = "iChat";
         else if (!strcmp (node.c_str(), "http://trillian.im/caps"))
             node = "Trillian";
         if (ext.empty())
