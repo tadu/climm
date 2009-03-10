7363b71906687513b6faf3304d0bedec9cfd76b7
Rüdiger Kuhlmann <tadu@users.noreply.github.com>
2009-03-10 14:24:54 +0000

    setssl_status to fail unless it was ok when closing (like in openssl)

--- src/io/io_gnutls.c
+++ src/io/io_gnutls.c
@@ -359,5 +359,5 @@ static void io_gnutls_close (Connection *conn, Dispatcher *d)
         EventD (QueueDequeue (conn, QUEUE_CON_TIMEOUT, conn->ip));
         free (d);
     }
-    conn->ssl_status = SSL_STATUS_NA;
+    conn->ssl_status = conn->ssl_status == SSL_STATUS_OK ? SSL_STATUS_NA : SSL_STATUS_FAILED;
 }
